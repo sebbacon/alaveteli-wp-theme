5e4eb3018f88e10faffd6de03f8c3dcb905818e5
Seb Bacon <seb.bacon@gmail.com>
2012-02-10 09:39:41 +0000

    New home page

---
+++ home.php
@@ -0,0 +1,73 @@
+<?php
+/**
+Template Name: Home page
+*/
+
+get_header(); ?>
+
+	<div id="general_frontpage" role="main">
+            
+  <div id="frontpage_splash">
+    <div id="left_column">
+      <h1>
+        Alaveteli is <strong>Social Email</strong><br>
+        <span> Make requests in public.
+Hold corporations and governments to account.</span>
+       </h1>
+      <a class="link_button_green_large" href="/about-2">Learn more »</a>
+    </div>
+    <div id="right_column">
+      <div id="frontpage_search_box">
+        <h2>
+	  Alaveteli has helped citizens ask more than<br>
+          <strong>100,000</strong> Freedom of Information requests in
+									 <strong>5</strong> jurisdictions, such as:
+        </h2>
+
+      </div>
+      <div id="frontpage_right_to_know">
+<a href="http://www.whatdotheyknow.com"><img src="<?php bloginfo('stylesheet_url'); ?>/../images/wdtk-logo.png"></a><br>
+<a href="http://www.asktheeu.org"><img src="<?php bloginfo('stylesheet_url'); ?>/../images/ateu-logo.png"></a>
+
+      </div>
+    </div>
+    <div style="clear:both"></div>
+  </div>
+
+<div id="frontpage_examples">
+    
+      <div id="examples_0">
+        <h3>What does Alaveteli do?</h3>
+        <ul>
+	  <li>Lower the barriers that prevent citizens asking questions of those in power</li>
+	  <li>Out-of-the-box support for making Freedom of Information requests to public bodies</li>
+	  <li>Sends requests via normal email channels, and publishes responses online (with attachments) instantly</li>
+          <li>Integrates well with social media</li>
+	  <li>Users can subscribe to email alerts matching any words or phrases they're interested in<</li>
+          <li>Responses are categorised by users for easy searching and reporting</li>
+	  <li>Translated into 8 languages (and counting...)</li>
+        </ul>
+
+      </div>
+    
+
+    <div id="examples_1">
+      <h3>How do I get started?</h3>
+        <ul>
+          
+            <li>
+               Download <a href="/asd">Turbo Transparency</a>, a paper about how and why to use Alaveteli for Freedom of Information systems</li>
+            <li>Find out more <a href="/about-2">about the Alaveteli project</a></li>
+            <li>Read the <a href="https://github.com/sebbacon/alaveteli/wiki/Getting-started">Getting Started guide</a> on our wiki</li>
+            <li>Developers might want to start with the <a href="https://github.com/sebbacon/alaveteli/wiki/Developers-guide">technical overview</a>.</a>
+        </ul>
+    </div>
+</div>
+
+
+
+
+            </div>
+        </dov>
+
+<?php get_footer(); ?>
